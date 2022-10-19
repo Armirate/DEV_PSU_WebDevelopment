b5235c729cd6de3dbf3097ad911bce8f100e7451
Armirate <sorawit.mattra@gmail.com>
2022-10-19 02:26:20 -0400

    Learned new tricks of JS and  JQuery

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-document.getElementById('demo').innerHTML = "new text";
-document.getElementById('demo').id = "demo2";
-document.getElementById('demo2').style.color = "red";
-document.getElementById('demo2').outerHTML = "<button> something to press </button>";
-
-function myFun(){
-    alert("WARNING ALERT OMG!!");
+$(document).ready(function() {
+    $("#btn1").mouseup( function() {
+        //$("#div1").slideToggle(2000);
+        let div = $("div");
+        div.animate({left:"400px", height:"300px", opacity:"0.5"},'slow');
+    }
+    );
 }
+);
 
-function darkMode(){
-    document.getElementById("pagestyle").setAttribute("href", "./style-dark.css");
-}
+// document.getElementById('demo').innerHTML = "new text";
+// // document.getElementById('demo').id = "demo2"; // should not chaange the id
+// document.getElementById('demo').style.color = "red";
+// // document.getElementById('demo2').outerHTML = "<button> something to press </button>";
 
-var a = 10;
-let b = 20;
-const c = 30;
+// function myFun(){
+//     alert("WARNING ALERT OMG!!");
+// }
 
-a += 10;
-b += 10;
-c += 10;
+// function darkMode(){
+//     document.getElementById("pagestyle").setAttribute("href", "./style-dark.css");
+// }
 
-console.log(a+b+c);
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// a += 10;
+// b += 10;
+
+// console.log(a+b+c);
+
+// function key_press(input){
+//     console.log(input);
+//     document.getElementById("keyPressed_text").innerHTML = input;
+// }
+
+// const key = function keyEvent(e){
+//     key_press(e.key);
+//     if (e.key === "f"){
+//         alert("f key pressed");
+//     }
+// }
+
+// document.addEventListener("keypress", key);
